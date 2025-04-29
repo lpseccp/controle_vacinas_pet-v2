3d66b30839182533f5aacf43a437038c22542ab0
lpseccp <lpseccp@gmail.com>
2025-04-29 10:18:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,26 @@
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open("pet-cache-v1").then(function (cache) {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
+const cacheName = 'pet-app-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/pet.html',
+  '/script.js',
+  '/pet.js',
+  '/style.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
     })
   );
 });
